e882be4c0351b41653ea7fbd4a094cd4b8cd7dc4
hungmung <hungmung@gmail.com>
2022-02-18 20:26:32 +0000

    Update ThreeSum.py

--- ThreeSum.py
+++ ThreeSum.py
@@ -1,5 +1,11 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        """
+        Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k,
+        and nums[i] + nums[j] + nums[k] == 0.
+        
+        Notice that the solution set must not contain duplicate triplets.
+        """
         if len(nums)<3: return []
         from itertools import combinations
         rtn = []
@@ -8,3 +14,4 @@ class Solution:
             if v not in rtn:
                 rtn.append(v)
         return rtn
+    # Note this solution is too slow and fails on test 315/318 on leetcode
